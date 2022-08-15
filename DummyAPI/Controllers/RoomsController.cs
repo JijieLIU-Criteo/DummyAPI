f6de5c7e20a2792261fd716bd8c099e1dcdceecd
ji.liu <ji.liu@criteo.com>
2022-08-15 18:19:59 +0200

    Add project files.

---
+++ DummyAPI/Controllers/RoomsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace DummyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetRooms()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DummyAPI/Controllers/RootController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DummyAPI.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class RootController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetRoot()
+        {
+            var response = new
+            {
+                message = "Hello world",
+            };
+            return Ok(response);
+        }
+    }
+}
