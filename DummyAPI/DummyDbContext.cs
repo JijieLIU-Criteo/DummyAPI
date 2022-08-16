e96e521d32c0b606d1e5606e2982cfbb6d4cd0b7
Jijie LIU <liujijieseason@gmail.com>
2022-08-16 22:03:45 +0200

    Create DummyDbContext.cs

---
+++ DummyAPI/DummyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using DummyAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DummyAPI
+{
+    public class DummyDbContext : DbContext
+    {
+        public DummyDbContext(DbContextOptions options)
+            : base(options) { }
+
+        public DbSet<RoomEntity> Rooms { get; set; }
+    }
+}
+
