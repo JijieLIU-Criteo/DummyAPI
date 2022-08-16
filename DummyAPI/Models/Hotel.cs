8b80b07287b8e6f2734489573b0e60adf5bc5ee9
Jijie LIU <liujijieseason@gmail.com>
2022-08-16 21:35:43 +0200

    Create Hotel.cs

---
+++ DummyAPI/Models/Hotel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DummyAPI.Models
+{
+    public class Hotel : Resource
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Eamil { get; set; }
+        public string Website { get; set; }
+        public Address address { get; set; }
+    }
+}
+
