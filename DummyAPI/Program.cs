14d79eee6c93d3c72afca5b0b8059c3b20c9ec6b
Jijie LIU <liujijieseason@gmail.com>
2022-08-16 23:09:17 +0200

    feat: seeding the in-memory db

--- DummyAPI/Program.cs
+++ DummyAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +14,9 @@ namespace DummyAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            InitializeDatabase(host);
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +25,25 @@ namespace DummyAPI
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        public static void InitializeDatabase(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.InitializeAsync(services).Wait();
+                }
+                catch (Exception e)
+                {
+                    var logger =
+                        services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(e,
+                        "An error occurred while seeding the database.");
+                }
+            }
+        }
     }
 }
---
+++ DummyAPI/SeedData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using DummyAPI.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DummyAPI
+{
+    public static class SeedData
+    {
+        public static async Task InitializeAsync(IServiceProvider service)
+        {
+            await AddTestData(service.GetRequiredService<DummyDbContext>());
+        }
+
+        public static async Task AddTestData(DummyDbContext context)
+        {
+            if (context.Rooms.Any())
+                return;
+
+            context.Rooms.Add(new RoomEntity
+            {
+                Id = Guid.Parse("DDE4BA55-808E-479F-BE8B-72F69913442F"),
+                Name = "AAA",
+                Rate = 12345
+            });
+
+            context.Rooms.Add(new RoomEntity
+            {
+                Id = Guid.Parse("DDE4BA55-808E-479F-BE8B-72F69913442E"),
+                Name = "BBB",
+                Rate = 98765
+            });
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
+
